ab0226a05c454fa2f3a89517e0ce0b06caa9d4b8
Aristo Hadisoeganda <aristo.hadisoeganda@gmail.com>
2024-09-19 22:56:36 +0700

    add --list and --purge

--- file_archiver.pl
+++ file_archiver.pl
@@ -7,21 +7,36 @@ use DateTime;
 use Getopt::Long;
 
 my @files = @ARGV;
-
-if(!@files) {
-  print "Usage : archiver [file1] [file2] ... [fileN]\n";
-  exit 3;
-}
-
-#GetOptions(
-#  'file=s{,}' => \@files,
-#) or die "Invalid options passed to $0\n";
-
-my $archiveDir = ".archives";
+my $currentDir = `pwd`;
+$currentDir =~ s/\n$//g;
+my $archiveDir = "$currentDir/.archives";
 my $now = DateTime->now;
 my $date = $now->ymd;
 my $time = $now->hms;
 my $datetime = "$date$time";
+
+if(!@files) {
+  print "Usage : archiver [option] -- [file1] [file2] ... [fileN]\n";
+  exit 3;
+}
+
+my $list = 0;
+my $purge = 0;
+GetOptions(
+  'list' => \$list,
+  'purge' => \$purge,
+) or die "Invalid options passed to $0\n";
+
+if($list) {
+  system "ls", $archiveDir;
+  exit 0;
+}
+
+if($purge) {
+  system "rm", "-rf", $archiveDir, "/*";
+  exit 0;
+}
+
 $datetime =~ s/[^0-9]//g;
 system("mkdir", "-p", $archiveDir);
 
@@ -29,7 +44,7 @@ foreach(@files) {
   my $file =  $_;
 
   my $archivedFile = "$archiveDir/[$datetime][$file]";
-  system("mv", "-f", $file, $archivedFile); 
-
-  print "File $file has been archived to $archivedFile\n";
+  if(!system("mv", "-f", $file, $archivedFile)) {
+    print "File $file has been archived to $archivedFile\n";
+  }
 }
